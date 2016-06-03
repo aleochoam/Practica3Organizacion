1fa16fec22d57c5423e5a00726fb02cd25e33fdd
Alejandro <alejo8a_2@hotmail.com>
2016-06-03 11:16:12 -0500

    Todo menos pasar a binario

--- Programa/Practica/hidemsg.asm
+++ Programa/Practica/hidemsg.asm
@@ -23,37 +23,40 @@ endstruc
 
 
 section .data
-    header   db  "P6",10
-    len_hed  equ $-header
-
-    header2  db  "255",10
-    len_hed2 equ $-header2
-
-
     fin     db 10
     fin_len equ $-fin
 
-    error_message: db "Se ha producido un error",10,0
-    error_message_length: equ $-error_message
+    error_message db "Se ha producido un error",10,0
+    error_message_length equ $-error_message
+
+    error_Argumentos db "Error con los argumentos",10,0
+    error_args_len equ $ - error_Argumentos
 
     fds dd 0            ;File descriptor de salida
     fde dd 0
-     zero equ 0
+
+    NUMARGS equ 6
 
 section .bss
     msg             resb 1024
-    msg_len         resb 28
-    num             resb 9
+    msg_len         resb 16
+
     stat            resb sizeof(STAT)
 
     nombreArchivoE  resw 2
     nombreArchivoS  resw 2
 
-    buffer:         resb 5242880
-    len_buffer      resb 1024
+    imagen:         resb 5242880
+    tam_imagen      resb 1024
+
+    numBin          resb 1024
+    numBin_len      resb 16
+
+    tam_header      resb 16
 
 section .text
     global _start
+
 
 ;Sacado de http://tuttlem.github.io/2013/01/08/strlen-implementation-in-nasm.html
 ;Metodo para calcular la longitud de un string
@@ -79,6 +82,25 @@ _strlen_null:
   pop   ecx            ; restore ecx
   ret                  ; get out
 
+
+_charToBin:
+    mov ecx, 7
+    mov edx, edi
+
+_nextBit:
+    shl bl, 1
+    setc byte[edi]
+    inc edi
+    dec ecx
+    jns _nextBit
+
+    mov byte[edi], 10
+    mov eax, edi
+    sub eax, edx
+    inc eax
+    ret
+
+
 _exit:
     mov eax, 1
     mov ebx, 0
@@ -95,56 +117,45 @@ _error:
     mov ebx, -1
     int 80h
 
+_error_Args:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, error_Argumentos
+    mov edx, error_message_length
+    int 80h
+    jmp _exit
+
+
 _start:
 
+;Numero de parametros
     pop ebx
-    pop ebx
-    pop ebx
-    mov [msg], ebx    ;Se saca el mensaje a esconder
 
-    pop ebx           ;Aca está el -f (puede que no sea asi)
+    cmp ebx, 6          ;Verifico el numero de argumentos
+    jne _error_Args
 
+;Nombre de programa
+    pop ebx             ;Saco el nombre del programa
+    pop ebx             ;Se saca el mensaje a esconder
+    mov [msg], ebx
+
+;Calculo la longitud del mensaje
+    mov edi, [msg]
+    call _strlen
+    mov [msg_len], eax
+
+;Analizo el -f
+    pop ebx              ;Aca está el -f (puede que no sea asi)
+    cmp byte[ebx], '-'
+    jne _error_Args
+    cmp byte[ebx+1], 'f'
+    jne _error_Args
+
+;Saco el nombre del archivo de entrada
     pop ebx
     mov [nombreArchivoE], ebx
 
-    pop ebx           ;Aca esta el -o (puede que no sea asi)
-
-    pop ebx
-    mov [nombreArchivoS], ebx
-    
-    mov ecx, 0
-
-_string_binario:
-    movzx eax, byte[msg + ecx]
-    ;mov eax, 4
-    cmp eax, 0
-
-_loop1:
-    mov edx, 0
-    cmp eax, 1
-    je  fin_binario
-    mov ebx, 2
-    div ebx
-    cmp edx, 0
-    je  es_cero
-    jmp es_uno
-
-es_cero:
-   mov ebx, 0
-   push ebx
-   jmp _loop1
-
-es_uno:
-   mov ebx, 1
-   push ebx
-   jmp _loop1
-
-fin_binario:
-   mov  ebx, 1
-   push ebx
-   inc  ecx
-   jmp _string_binario
-    
+;Obtengo el tamaño de la imagen
 _obtener_tamano_archivo:
     mov eax, 106
     mov ebx, nombreArchivoE
@@ -152,7 +163,19 @@ _obtener_tamano_archivo:
     int 80h
 
     mov eax, dword [stat + STAT.st_size]
-    mov [len_buffer], eax
+    mov [tam_imagen], eax
+
+;Analizo el -o
+    pop ebx           ;Aca esta el -o (puede que no sea asi)
+    cmp byte[ebx], '-'
+    jne _error_Args
+    cmp byte[ebx+1], 'o'
+    jne _error_Args
+
+;Saco el nombre del archivo de salida
+    pop ebx
+    mov [nombreArchivoS], ebx
+
 
 _abrir_archivo_de_entrada:
     mov eax,5
@@ -164,21 +187,30 @@ _abrir_archivo_de_entrada:
 _leer_archivo_de_entrada:
     mov eax, 3
     mov ebx,[fde]
-    mov ecx,buffer
-    mov edx, len_buffer
+    mov ecx,imagen
+    mov edx, tam_imagen
     int 80h
     js _error
 
-;    mov ebx, 1
-;    mov ecx, buffer
-;    mov edx, len_buffer
-;    mov eax, 4
-;    int 80h
-
-_cerrar_archivo_de_entrada:
-    mov ebx,[fde]
     mov eax, 6
+    mov ebx,[fde]
     int 80h
+
+    jmp _crear_archivo_salida
+; Convertir a binario
+    mov esi, [msg]
+    mov edi, numBin
+
+_strToBin:
+    cmp byte[esi], 0
+    je _crear_archivo_salida ;CAMBIAR ---------------------------------
+
+    mov bl, byte[esi]
+    call _charToBin
+
+    inc esi
+    inc edi
+    jmp _strToBin
 
 _crear_archivo_salida:
     mov eax, 8
@@ -188,46 +220,37 @@ _crear_archivo_salida:
 
     mov [fds], eax
 
+
+_encontrarImagen:
+    mov ecx, imagen
+    mov ebx, 0                  ;indicador fin Lde header
+    mov edx, 0                  ;contador fin de linea
+
+_cicloHeader:
+    cmp byte[ecx], 10
+    je _finDeLinea
+    inc ebx
+    inc ecx;
+    jmp _cicloHeader
+
+_finDeLinea:
+    inc edx
+    cmp edx, 3
+    je _finHeader
+    inc ebx
+    inc ecx
+    jmp _cicloHeader
+
+_finHeader:
+    mov [tam_header], ebx
+
+_cerrar_Archivo_Salida:
     mov eax, 4
     mov ebx, [fds]
-    mov ecx, header
-    mov edx, len_hed
+    mov ecx, imagen
+    mov edx, [tam_header]
     int 80h
 
-    cmp eax, -1
-    je _error
-
-    ;Agregar el la cantidad de pixeles
-
-    mov eax, 4
-    mov ebx, [fds]
-    mov ecx, header2
-    mov edx, len_hed2
-    int 80h
-
-    cmp eax, -1
-    je _error
-
-_agregar_el_mensaje:
-    mov edi, [msg]
-    call _strlen
-    mov [msg_len], eax
-
-    mov eax, 4
-    mov ebx, [fds]
-    mov ecx, [msg]
-    mov edx, [msg_len]
-    int 80h
-    
-    mov ebx, [fds]
-    mov ecx, buffer
-    mov edx, len_buffer
-    mov eax, 4
-    int 80h
-
-
-
-_cerrar_Archivo:
     mov eax, 4
     mov ebx, [fds]
     mov ecx, fin
