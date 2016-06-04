783fffec156dfc4ab4ed41b328b48f1d24889c43
Felipe <fmontoyalvarez@gmail.com>
2016-06-04 11:26:44 -0500

    Imagen nueva

--- Programa/Practica/hidemsg.asm
+++ Programa/Practica/hidemsg.asm
@@ -63,24 +63,24 @@ section .text
 
 _strlen:
 
-  push  ecx            ; save and clear out counter
-  xor   ecx, ecx
+   push  ecx            ; save and clear out counter
+   xor   ecx, ecx
 
 _strlen_next:
 
-  cmp   [edi], byte 0  ; null byte yet?
-  jz    _strlen_null   ; yes, get out
+   cmp   [edi], byte 0  ; null byte yet?
+   jz    _strlen_null   ; yes, get out
 
-  inc   ecx            ; char is ok, count it
-  inc   edi            ; move to next char
-  jmp   _strlen_next   ; process again
+   inc   ecx            ; char is ok, count it
+   inc   edi            ; move to next char
+   jmp   _strlen_next   ; process again
 
 _strlen_null:
 
-  mov   eax, ecx       ; ecx = the length (put in eax)
+   mov   eax, ecx       ; ecx = the length (put in eax)
 
-  pop   ecx            ; restore ecx
-  ret                  ; get out
+   pop   ecx            ; restore ecx
+   ret                  ; get out
 
 
 _charToBin:
@@ -197,14 +197,49 @@ _leer_archivo_de_entrada:
     int 80h
 
     jmp _crear_archivo_salida
-; Convertir a binario
+   
+    mov ecx, 0
+ 
+;_string_binario:
+;    movzx eax, byte[msg + ecx]
+;    cmp eax, byte 0
+;    je finbin
+ 
+;_loop1:
+;    mov edx, 0
+;    cmp eax, 1
+;    je  fin_loop1
+;    mov ebx, 2
+;    div ebx
+;    cmp edx, 0
+;    je  es_cero
+;    jmp es_uno
+ 
+;es_cero:
+;    mov ebx, 0
+;    push ebx
+;    jmp _loop1
+
+;es_uno:
+;    mov ebx, 1
+;    push ebx
+;    jmp _loop1
+ 
+;fin_loop1:
+;    mov  ebx, 1
+;    push ebx
+;    inc  ecx
+;    jmp _string_binario
+;finbin:
+;    jmp _crear_archivo_salida
+
+ Convertir a binario
     mov esi, [msg]
     mov edi, numBin
 
 _strToBin:
     cmp byte[esi], 0
     je _crear_archivo_salida
-
     mov bl, byte[esi]
     call _charToBin
 
@@ -243,7 +278,8 @@ _finDeLinea:
 
 _finHeader:
     mov [tam_header], ebx
-
+;_ciclo_escribir:
+;    mov eax, 
 _cerrar_Archivo_Salida:
     mov eax, 4
     mov ebx, [fds]
